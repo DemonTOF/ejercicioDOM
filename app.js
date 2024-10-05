c9d6977384a2846668217f111dd15d27266be838
DemonTOF <santi23py@gmail.com>
2024-10-05 01:13:33 -0400

    entrega tarea Selector de Color RGB Interactivo

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 // Selecciona los elementos del DOM.
-const inputRojo = document.getElementById('rojo');
+const inputRojo = document.getElementById('rojo');   // input range rojo
 const inputVerde = document.getElementById('verde');
 const inputAzul = document.getElementById('azul');
 
-const textoRojo = document.getElementById('texto-rojo');
+const textoRojo = document.getElementById('texto-rojo');    // p rojo
 const textoVerde = document.getElementById('texto-verde');
 const textoAzul = document.getElementById('texto-azul');
 
-// Valores iniciales para definir el color RGB.
-let rojo = inputRojo.value;
-let verde = inputVerde.value;
-let azul = inputAzul.value;
-
-// Muestra los valores iniciales en los elementos <p>.
-textoRojo.textContent = rojo;
-textoVerde.textContent = verde;
-textoAzul.textContent = azul;
 
 // Función para actualizar el color de fondo de la página.
-function actualizarColor(rojo, verde, azul) {
+function actualizarColor() {
   // Completa esta función para cambiar el color de fondo.
+  // Valores iniciales para definir el color RGB.
+  let rojo = inputRojo.value;      //  value="23"
+  let verde = inputVerde.value;
+  let azul = inputAzul.value;
+
+  // Muestra los valores iniciales en los elementos <p>.
+  textoRojo.textContent = rojo;  // <p id="texto-rojo">23</p>
+  textoVerde.textContent = verde;
+  textoAzul.textContent = azul;
+
+  const colorCaja = document.querySelector("body").style.backgroundColor = `rgb(${rojo}, ${verde}, ${azul})`;
 }
 
 // Agrega un event listener para el input de Rojo.
-inputRojo.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputRojo.addEventListener('input', (e) => {
+  actualizarColor()
 });
 
 // Agrega un event listener para el input de Verde.
-inputVerde.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputVerde.addEventListener('input', (e) => {
+  actualizarColor()
 });
 
 // Agrega un event listener para el input de Azul.
-inputAzul.addEventListener('change', (e) => {
-  // Actualiza el valor de la variable y el texto.
-  // Llama a la función para actualizar el color de fondo.
+inputAzul.addEventListener('input', (e) => {
+  actualizarColor()
 });
+
+actualizarColor();
